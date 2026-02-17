eba180c40887b3651178c09c2e2b8f239e7ffb73
chaithnya teja <159903572+teja-able@users.noreply.github.com>
2026-02-17 19:51:17 +0530

    chore: fix bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import bgImage from '$lib/assets/coming soon.avif';
+</script>
+
+<div 
+  class="w-screen h-screen bg-cover bg-center"
+  style="background-image: url('{bgImage}')">
+</div>
